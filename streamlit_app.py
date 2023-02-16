d5a8946f8e8952522076cdadbedd0874bcce8133
JhansiGushidi <125554434+JhansiGushidi@users.noreply.github.com>
2023-02-16 16:29:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,3 @@
-import streamlit
-
-streamlit.title("My Parents New Healthy Diner")
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text(' 🐔 Hard-Bstreamlitoiled Free-Range Egg')
-streamlit.text('🥑🍞Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dadw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
