67914717704800652c28e1ad83b277f16ffdf107
JhansiGushidi <125554434+JhansiGushidi@users.noreply.github.com>
2023-04-06 11:24:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,7 @@ fruits_to_show  = my_fruit_list.loc[fruits_selected]
 # display the table on the page
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" "+ kiwi")
-# write your own comment -what does the next line do? 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
